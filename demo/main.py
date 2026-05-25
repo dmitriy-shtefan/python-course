7766bb39a6f44f1f2d5ba297bccedfe11eaec3d3
Dmitriy Shtefan <dmitriy.shtefan@gameloft.com>
2026-05-25 15:48:03 +0300

    Add day 5 materials

---
+++ demo/main.py
@@ -0,0 +1,25 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.color("red", "blue")
+turtle.speed(1)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+
+turtle.circle(20)
+
+turtle.penup()
+
+turtle.backward(100)
+
+turtle.pendown()
+
+turtle.circle(20)
+
+
+turtle.done()
